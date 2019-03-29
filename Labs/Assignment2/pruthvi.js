c9ba100f5342eda830a4c80e601f91ed4b038c02
Pruthvi <pruthvi@users.noreply.github.com>
2019-03-29 00:35:55 -0400

    Cleaned the code and updated raycast

--- Labs/Assignment2/pruthvi.js
+++ Labs/Assignment2/pruthvi.js
@@ -1,20 +1,23 @@
+/*  
+    Copyright Pruthvi // pruthv.com 
+    https://pruthvi.github.io/Advance_Graphics/Labs/Assignment2/assignment2.html
+    https://codepen.io/pruthv/pen/pYMpda 
+*/
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 const scene = new Physijs.Scene({ reportsize: 50, fixedTimeStep: 1 / 60 });
 const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1.0, 1000);
 
 var orbitControls, controls;
-
 var ambientLight, dirLight;
-var rotateScene, speedX, speedY, speedZ;
+var axle = new THREE.Object3D();
 
+var texture;
 var objects = [];
+var rotateScene, speedX, speedY, speedZ;
 
 var score = 0, lostBox = 0;
 var text, instruText, lostText;
-var axle = new THREE.Object3D();
-var texture;
-var spotlightHepler;
 
 Physijs.scripts.worker = 'libs/physijs_worker.js';
 Physijs.scripts.ammo = 'libs/ammo.js';
@@ -27,12 +30,11 @@ function init() {
     renderer.setClearColor(0x004400);
     renderer.shadowMap.enabled = true;
 
-
     scene.setGravity(new THREE.Vector3(0, -50, 0)); //specify gravity
+
     stats = new Stats();
     stats.showPanel(0); // 0: fps, 1: ms, 2: mb, 3+: custom
     //document.body.appendChild(stats.dom);
-
 
     rotateScene = true;
     speedX = 0;
@@ -59,17 +61,15 @@ function setupCameraAndLight() {
     dirLight.castShadow = true;
     scene.add(dirLight);
 
-
-
 }
 
 function createGeometry() {
 
     scene.add(new THREE.AxesHelper(10));
 
+    // Score Text
     text = document.createElement('div');
     text.style.position = 'absolute';
-    text.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
     text.style.width = 100;
     text.style.height = 100;
     text.style.backgroundColor = "white";
@@ -79,25 +79,11 @@ function createGeometry() {
     text.style.left = 50 + 'px';
     document.body.appendChild(text);
 
-
-    instruText = document.createElement('div');
-    instruText.style.fontFamily = 'sans-serif';
-    instruText.style.position = 'absolute';
-    instruText.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
-    instruText.style.width = 100;
-    instruText.style.height = 100;
-    instruText.style.color = "white";
-    instruText.style.fontSize = "14px";
-    instruText.style.bottom = 20 + 'px';
-    instruText.style.right = 20 + 'px';
-    instruText.innerHTML = "+10 points for removing block <br />  -10 points if block falls off table  <br /> Click anywhere to change rotation.";
-    document.body.appendChild(instruText);
-
+    // Level Lost Boxes Text
     lostText = document.createElement('div');
     lostText.style.fontFamily = 'sans-serif';
     lostText.style.textAlign = 'center';
     lostText.style.position = 'absolute';
-    lostText.style.zIndex = 1;    // if you still don't see the label, try uncommenting this
     lostText.style.width = 100;
     lostText.style.height = 100;
     lostText.style.backgroundColor = "black";
@@ -107,103 +93,58 @@ function createGeometry() {
     lostText.style.left = 50 + 'px';
     document.body.appendChild(lostText);
 
+    // Instruction Text
+    instruText = document.createElement('div');
+    instruText.style.fontFamily = 'sans-serif';
+    instruText.style.position = 'absolute';
+    instruText.style.width = 100;
+    instruText.style.height = 100;
+    instruText.style.color = "white";
+    instruText.style.fontSize = "14px";
+    instruText.style.bottom = 20 + 'px';
+    instruText.style.right = 20 + 'px';
+    instruText.innerHTML = "+10 points for removing block <br />  -10 points if block falls off table  <br /> Click anywhere to change rotation.";
+    document.body.appendChild(instruText);
 
-
-    // load a texture, set wrap mode to repeat
+    // Load table texture, set wrap mode to repeat
     texture = new THREE.TextureLoader().load("assets/textures/wood.jpg");
     texture.wrapS = THREE.RepeatWrapping;
     texture.wrapT = THREE.RepeatWrapping;
     texture.repeat.set(4, 4);
 
-
+    // Initial Scene Floor
     let floor = new Physijs.BoxMesh(
         new THREE.CubeGeometry(20, 1, 10),
         new THREE.MeshBasicMaterial({ color: 0xeeeeee, map: texture })
         , 0);
     floor.position.set(0, 0, 0);
-     scene.add(floor);
+    scene.add(floor);
 
 }
 
-
-function createBox(size, boxColor, x, y, z) {
-
-    let boxGeometry = new THREE.BoxGeometry(size, size, size);
-    var boxMaterial = Physijs.createMaterial(
-        new THREE.MeshStandardMaterial({ color: boxColor }),
-        0.5, 0);
-    let box = new Physijs.BoxMesh(boxGeometry, boxMaterial);
-
-    box.position.set(x, y, z);
-    box.castShadow = true;
-    box.receiveShadow = true;
-    objects.push(box);
-    scene.add(box);
-
-}
-
-function readFile(port, filename) {
-    let url = 'http://127.0.0.1:' +
-        port + //port number from data.gui
-        '/assets/games/' + //url path
-        filename + //file name from dat.gui
-        '.json'; //extension
-    let onlineurl = '/Advance_Graphics/Labs/Assignment2/assets/games/' +  filename + '.json';
-    let request = new XMLHttpRequest();
-    // console.log("URL : " + onlineurl);
-    request.open('GET', onlineurl);
-    request.responseType = 'text'; //try text if this doesn’t work
-    request.send();
-    request.onload = () => {
-        let data = request.responseText;
-console.log(data); //debugging code
-createGame(data);
-      //  createGame(JSON.parse(data)); //convert text to json
-    }
-}
-
-function createGame(data) {
-    console.log(data.setting.rotation_y);
-    console.log(data);
-
-    speedX = parseFloat(data.setting.rotation_x);
-    speedY = parseFloat(data.setting.rotation_y);
-    speedZ = parseFloat(data.setting.rotation_z);
-
-    var count = Object.keys(data.info).length;
-    for (i = 0; i < count; i++) {
-        var iSize = data.info[i].size;
-        var iColor = parseInt(data.info[i].color, 16);
-        var iX = data.info[i].x;
-        var iY = data.info[i].y;
-        var iZ = data.info[i].z;
-        createBox(iSize, iColor, iX, iY, iZ);
-    }
-
-}
 
 function setupDatGui() {
 
     controls = new function () {
-        this.lvl1 = function () {
+
+        this.lvl1 = function () {       // clears scene
             while (scene.children.length > 0) {
                 scene.remove(scene.children[0]);
             }
-            lostBox = 0;
-            readFile(5500, "lvl1");
-
-            createFloor(5, 0);
-
+            lostBox = 0;    // resets 'level lost box' count
+            readFile(5500, "lvl1"); // loads json file
+            createFloor(5, 0);  // adds static scene elements
         };
+
         this.lvl2 = function () {
             while (scene.children.length > 0) {
                 scene.remove(scene.children[0]);
             }
             lostBox = 0;
             createFloor(7, 90);
-
             readFile(5500, "lvl2");
         };
+
         this.lvl3 = function () {
             while (scene.children.length > 0) {
                 scene.remove(scene.children[0]);
@@ -212,6 +153,7 @@ function setupDatGui() {
             createFloor(1.75, 0);
             readFile(5500, "lvl3");
         };
+
         this.lvl4 = function () {
             while (scene.children.length > 0) {
                 scene.remove(scene.children[0]);
@@ -219,9 +161,9 @@ function setupDatGui() {
             lostBox = 0;
             createFloor(2, 0);
             createFloor(2, 90);
-
             readFile(5500, "lvl4");
         };
+
         this.lvl5 = function () {
             while (scene.children.length > 0) {
                 scene.remove(scene.children[0]);
@@ -229,7 +171,6 @@ function setupDatGui() {
             lostBox = 0;
             createFloor(2, 0);
             createFloor(2, 90);
-
             readFile(5500, "lvl5");
         };
 
@@ -242,7 +183,6 @@ function setupDatGui() {
     gui.add(controls, 'lvl4').name('Level 4');
     gui.add(controls, 'lvl5').name('Level 5');
 
-
 }
 
 function createFloor(z, angle) {
@@ -250,13 +190,57 @@ function createFloor(z, angle) {
     scene.add(ambientLight);
     let floor = new Physijs.BoxMesh(
         new THREE.CubeGeometry(20, 1, z),
-        new THREE.MeshStandardMaterial({ color: 0xeeeeee, map: texture })
-        , 0);
+        new THREE.MeshStandardMaterial({ color: 0xeeeeee, map: texture }), 0);
     floor.position.set(0, 0, 0);
     floor.rotation.y = angle;
     floor.castShadow = true;
     floor.receiveShadow = true;
     scene.add(floor);
+}
+
+function readFile(port, filename) {
+
+    let url = 'http://127.0.0.1:' + // Change link to localhost if required
+        port + '/assets/games/' + filename + '.json';
+    // let onlineurl = 'https://pruthvi.github.io/Advance_Graphics/Labs/Assignment2/assets/games/' +  filename + '.json'; /* Online Link */
+    let request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.send();
+    request.onload = () => {
+        let data = request.responseText;
+        //  createGame(data);               // for online link
+        createGame(JSON.parse(data));
+    }
+}
+
+function createGame(data) {
+
+    speedX = parseFloat(data.setting.rotation_x);   // Sets Rotation
+    speedY = parseFloat(data.setting.rotation_y);
+    speedZ = parseFloat(data.setting.rotation_z);
+
+    var count = Object.keys(data.info).length;
+    for (i = 0; i < count; i++) {
+        var iSize = data.info[i].size;
+        var iColor = parseInt(data.info[i].color, 16);
+        var iX = data.info[i].x;
+        var iY = data.info[i].y;
+        var iZ = data.info[i].z;
+        createBox(iSize, iColor, iX, iY, iZ);
+    }
+}
+
+function createBox(size, boxColor, x, y, z) {
+
+    let boxGeometry = new THREE.BoxGeometry(size, size, size);
+    var boxMaterial = Physijs.createMaterial(new THREE.MeshStandardMaterial({ color: boxColor }), 0.5, 0);
+    let box = new Physijs.BoxMesh(boxGeometry, boxMaterial);
+    box.position.set(x, y, z);
+    box.castShadow = true;
+    box.receiveShadow = true;
+    objects.push(box);
+    scene.add(box);
 }
 
 function mouseDownHandler(event) {
@@ -266,75 +250,68 @@ function mouseDownHandler(event) {
         -(event.clientY / window.innerHeight) * 2 + 1,
         0.5
     );
-    console.log(pos);
     let vector = pos.unproject(camera);
     let raycaster = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
     let intersects = raycaster.intersectObjects(objects);
-    intersects.forEach((obj) => {
-        scene.remove(obj.object);
-        score = score + 10;
-    });
+    console.log("Intersects : " + intersects);
+    console.log("Length : " + intersects.length);
+    if (intersects.length > 0) {
+        var firstElement = intersects[0].object;
+        console.log("Remove : " + firstElement);
+        scene.remove(firstElement);
+    }
 
+
+
+    // intersects.forEach((obj) => {
+    //     scene.remove(obj.object);
+    //     score = score + 10;
+    // });
     objects.forEach(obj => {
         obj.__dirtyPosition = true;
         obj.__dirtyRotation = true;
-
     });
 
 }
 
-
 function checkLocation() {
 
+    text.innerHTML = " &nbsp;&nbsp;Score : " + score + "&nbsp;&nbsp;";          // Checks score on each frame
+    lostText.innerHTML = "Level <br />  &nbsp;&nbsp;Lost Boxes : " + lostBox + "&nbsp;&nbsp;";  // Checks lost box on each frame
 
-    text.innerHTML = " &nbsp;&nbsp;Score : " + score + "&nbsp;&nbsp;";
-    lostText.innerHTML = "Level <br />  &nbsp;&nbsp;Lost Boxes : " + lostBox + "&nbsp;&nbsp;";
-
-
-    for (let i = 0; i < objects.length; i++) {
+    for (let i = 0; i < objects.length; i++) {      // Checks if box fell from certain height and reduce points accordingly
         if (objects[i].position.y <= -10) {
             scene.remove(objects[i]);
             objects.splice(i, 1);
             lostBox = lostBox + 1;
-
             score = score - 10;
         }
     }
-
 }
 
 function render() {
-    if (rotateScene == true) {
 
+    if (rotateScene == true) {          // Rotates scene according to level
         scene.rotation.x += speedX;
         scene.rotation.y += speedY;
         scene.rotation.z += speedZ;
     }
 
-    stats.update();
-    checkLocation();
+    checkLocation();    // check movement of box and update stat
 
+    // stats.update();       // For updating stats and fps
     scene.simulate();
-
     orbitControls.update();
-
-
-
-    renderer.render(scene, camera);
-    // scene.simulate(undefined, 1);
     renderer.castShadow = true;
-
+    renderer.render(scene, camera);
     requestAnimationFrame(render);
 }
 
 window.onload = () => {
-
     init();
     setupCameraAndLight();
     createGeometry();
     setupDatGui();
     window.addEventListener('mousedown', mouseDownHandler, false);
-
     render();
-
 }
